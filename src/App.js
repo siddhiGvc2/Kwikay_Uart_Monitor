cd776c5f291b0688c916127b1ce15fc2b66c8b39
siddhiGvc2 <sd@gvc.in>
2025-09-10 11:20:41 +0530

    TCP errors counted

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
     ssid: "",
     hbt_counter:0,
     hbt_timer:0,
-    wifi_erros:0,
+    wifi_errors:0,
     tcp_errors:0,
     mqtt_errors:0
   });
@@ -33,7 +33,7 @@ export default function App() {
   // Parse terminal data into device info
   const parseDeviceInfo = (data) => {
     console.log(data);
-  const info = { macId: "", fwVersion: "", serialNumber: "", ssid: "" ,hbt_timer:0,hbt_counter:0, wifi_erros:0,tcp_errors:0,mqtt_errors:0};
+  const info = { macId: "", fwVersion: "", serialNumber: "", ssid: "" };
 
  if (data.startsWith("*MAC:")) {
       console.log(data);
@@ -61,11 +61,17 @@ export default function App() {
     else if(data.startsWith("*HBT-")){
       console.log(data);
       setDeviceInfo((prev) => ({
-    ...prev,
-    hbt_counter: (prev.hbt_counter || 0) + 1,
-    hbt_timer: 0,
-  }));
+        ...prev,
+        hbt_counter: (prev.hbt_counter || 0) + 1,
+        hbt_timer: 0,
+      }));
        
+    }
+    else if(data.startsWith("*TCP-NOTOK#")){
+      setDeviceInfo((prev) => ({
+        ...prev,
+        tcp_errors: (prev.tcp_errors || 0) + 1,
+      }));
     }
 
 
@@ -76,7 +82,12 @@ export default function App() {
     fwVersion: info.fwVersion || prev.fwVersion,
     ssid: info.ssid || prev.ssid,
     hbt_counter:info.hbt_counter || prev.hbt_counter,
-    hbt_timer:info.hbt_timer || prev.hbt_timer
+    hbt_timer:info.hbt_timer || prev.hbt_timer,
+    wifi_erros:info.wifi_erros || prev.wifi_errors,
+    tcp_errors:info.tcp_errors || prev.tcp_errors,
+    mqtt_errors:info.mqtt_errors || prev.mqtt_errors
+    
+
   }));
 };
 
@@ -86,14 +97,10 @@ useEffect(()=>{
   if(status=="Connected")
   {
     Interval=setInterval(()=>{
-    setDeviceInfo((prev) => ({
-    macId: prev.macId,
-    serialNumber: prev.serialNumber,
-    fwVersion: prev.fwVersion,
-    ssid: prev.ssid,
-    hbt_counter:prev.hbt_counter,
-    hbt_timer:prev.hbt_timer+1,
-  }));
+     setDeviceInfo((prev) => ({
+        ...prev,
+        hbt_timer: (prev.hbt_timer || 0) + 1,
+      }));
   },1000);
   }
   else{
@@ -266,13 +273,13 @@ let uartBuffer = "";
              <strong>HBT-S:</strong>  {deviceInfo.hbt_counter} / {deviceInfo.hbt_timer}
            </div>
               <div className="info-card">
-             <strong>WIFI-ERRORS:</strong>  
+             <strong>WIFI-ERRORS:</strong> {deviceInfo.wifi_errors}
            </div>
             <div className="info-card">
-             <strong>TCP-ERRORS:</strong>  
+             <strong>TCP-ERRORS:</strong> {deviceInfo.tcp_errors}
            </div>
             <div className="info-card">
-             <strong>MQTT-ERRORS:</strong>  
+             <strong>MQTT-ERRORS:</strong> {deviceInfo.mqtt_errors}
            </div>
            
         </div>
